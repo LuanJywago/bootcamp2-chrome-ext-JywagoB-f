158648e4833a8fa9094ae7d007a1a65b061f0fba
JywagoB&F <luanjywago2015@gmail.com>
2025-10-19 14:10:25 -0300

    Update playwright.config.ts

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -2,11 +2,11 @@ import { defineConfig, devices } from '@playwright/test';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-// Corrige __dirname em módulos ES
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Caminho para a pasta dist da extensão
+
 const distPath = path.join(__dirname, '..', 'dist');
 
 export default defineConfig({
@@ -16,7 +16,7 @@ export default defineConfig({
     ['html', { outputFolder: 'playwright-report', open: 'never' }]
   ],
   use: {
-    headless: true, // use false se quiser ver o navegador abrindo
+    headless: true,
   },
   projects: [
     {
